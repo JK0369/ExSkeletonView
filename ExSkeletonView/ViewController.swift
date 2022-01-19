fbae520e144c5b2809938dd85e8545c6a8231178
jake.k <jake.k@hpcnt.com>
2022-01-19 20:37:37 +0900

    Add label

--- ExSkeletonView/ViewController.swift
+++ ExSkeletonView/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
     $0.register(MyCell.self, forCellReuseIdentifier: "cell")
     $0.backgroundColor = .black
   }
+  private let label = UILabel().then {
+    $0.text = "123456577765"
+    $0.numberOfLines = 0
+    $0.textColor = .white
+    $0.isSkeletonable = true
+  }
   private var dataSource = [MyData]()
   
   override func viewDidLoad() {
@@ -22,17 +28,30 @@ class ViewController: UIViewController {
     
     self.view.backgroundColor = .black
     self.view.addSubview(self.tableView)
+    self.view.addSubview(self.label)
     
+    self.label.snp.makeConstraints {
+      $0.centerX.equalToSuperview()
+      $0.top.equalToSuperview().inset(52)
+    }
     self.tableView.snp.makeConstraints {
-      $0.edges.equalToSuperview()
+      $0.top.equalToSuperview().inset(120)
+      $0.left.right.bottom.equalToSuperview()
     }
     
     self.tableView.dataSource = self
     
-    self.tableView.isSkeletonable = true
-    self.tableView.showSkeleton(usingColor: .gray, transition: .crossDissolve(1))
+    let skeletonAnimation = SkeletonAnimationBuilder().makeSlidingAnimation(withDirection: .leftRight)
+//    self.label.showSkeleton(usingColor: .gray)
+    /// transition: 스켈레톤 보여질때와 사라질때 효과
+    self.label.showAnimatedGradientSkeleton(usingGradient: .init(colors: [.lightGray, .gray]), animation: skeletonAnimation, transition: .none)
+    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+      self.label.hideSkeleton()
+    }
+//    self.tableView.isSkeletonable = true
+//    self.tableView.showSkeleton(usingColor: .gray, transition: .crossDissolve(1))
     
-    self.fetchDataSource()
+//    self.fetchDataSource()
   }
   
   private func fetchDataSource() {
